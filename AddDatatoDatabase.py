1293f8ccd77c04a698f8506b6fc9fa745f502e1f
atul120212 <atulsharma28092004@gmail.com>
2023-12-19 10:36:26 +0530

    first commit

---
+++ AddDatatoDatabase.py
@@ -0,0 +1,66 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://faceattendance-1ae9c-default-rtdb.firebaseio.com/"
+})
+
+ref = db.reference('Students')
+
+data = {
+    "321654":
+        {
+            "name": "Murtaza Hassan",
+            "major": "Robotics",
+            "starting_year": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "852741":
+        {
+            "name": "Emly Blunt",
+            "major": "Economics",
+            "starting_year": 2021,
+            "total_attendance": 12,
+            "standing": "B",
+            "year": 1,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "963852":
+        {
+            "name": "Elon Musk",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+        "324432":
+        {
+            "name": "Atul Sharma",
+            "major": "Robotics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2023-12-14 00:54:34"
+        },
+        "112211":
+        {
+            "name": "Harsh Gupta",
+            "major": "Robo",
+            "starting_year": 2021,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2023-12-14 00:54:34"
+        },
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
